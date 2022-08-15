8cb75b73a9495a5329b403dde02e612ff1c75d16
mandara11 <mandaram2345@gmail.com>
2022-08-15 22:24:13 +0530

    CheckAnagram

--- Program.cs
+++ Program.cs
@@ -11,13 +11,16 @@
             //BubbleSort bubbleSort = new BubbleSort();
             //bubbleSort.Sorting();
 
-            MergeSort mergeSort = new MergeSort();
-            int[] arr4 = { 12, 11, 13, 5, 6, 7 };
-            Console.WriteLine("Unsorted array");
-            mergeSort.DisplayMerge(arr4);
-            mergeSort.Sorting(arr4, 0, arr4.Length - 1);
-            Console.WriteLine("unsorted array");
-            mergeSort.DisplayMerge(arr4);
+            //MergeSort mergeSort = new MergeSort();
+            //int[] arr4 = { 12, 11, 13, 5, 6, 7 };
+            //Console.WriteLine("Unsorted array");
+            //mergeSort.DisplayMerge(arr4);
+            //mergeSort.Sorting(arr4, 0, arr4.Length - 1);
+            //Console.WriteLine("unsorted array");
+            //mergeSort.DisplayMerge(arr4);
+
+            Anagram anagram = new Anagram();
+            anagram.checkAnagram();
         }
     }
 }
