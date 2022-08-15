c2e5bb46723403ab4ee835d632accb30ec93ef5c
mandara11 <mandaram2345@gmail.com>
2022-08-15 22:16:08 +0530

    Added:Mergesort

--- Program.cs
+++ Program.cs
@@ -4,12 +4,20 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Problem");
-            InsertionSort insertionSortDemo = new InsertionSort();
-            insertionSortDemo.CheckSorting();
+            //Console.WriteLine("Welcome to Algorithm Problem");
+            //InsertionSort insertionSortDemo = new InsertionSort();
+            //insertionSortDemo.CheckSorting();
 
-            BubbleSort bubbleSort = new BubbleSort();
-            bubbleSort.Sorting();
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.Sorting();
+
+            MergeSort mergeSort = new MergeSort();
+            int[] arr4 = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("Unsorted array");
+            mergeSort.DisplayMerge(arr4);
+            mergeSort.Sorting(arr4, 0, arr4.Length - 1);
+            Console.WriteLine("unsorted array");
+            mergeSort.DisplayMerge(arr4);
         }
     }
 }
